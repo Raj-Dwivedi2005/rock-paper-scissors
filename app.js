2cf7f21fb82862f5a1012723afe73c81c869c78c
Raj-Dwivedi2005 <rajiaf202704@gmail.com>
2025-06-10 23:41:34 +0530

    Updated

--- app.js
+++ app.js
@@ -3,9 +3,17 @@ let compScore = 0;
 
 const choices = document.querySelectorAll(".choice");
 
+const genCompChoice = () => {
+    
+}
+const playGame = (userchoice) => {
+    console.log("user choice = ", userchoice);
+
+};
+
 choices.forEach((choice) => {
     choice.addEventListener("click", () => {
-        const choiceId = choice.getAttribute("id");
-        console.log("choice was clicked", choiceId);
+        const userchoice = choice.getAttribute("id");
+        playGame(userchoice);
     });
 });
